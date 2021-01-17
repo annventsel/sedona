fc25b5f5947415e74e3cef31568b69c60373b2b3
annventsel <aventsel.15@gmail.com>
2021-01-17 21:05:49 +0200

    сборка

---
+++ source/js/script.js
@@ -0,0 +1,4 @@
+// var navMain = document.querySelector(".main-nav"),
+// navToggle = document.querySelector(".main-nav__toggle");
+// navMain.classList.contains("main-nav--closed") || (navToggle.classList.add("main-nav--closed"), navMain.classList.add("main-nav--closed")),
+// navMain.addEventListener("click", function(event) {event.preventDefault(),navToggle.classList.contains("main-nav--closed")?(navToggle.classList.remove("main-nav--closed"),navMain.classList.remove("main-nav--closed")): (navToggle.classList.add("main-nav--closed"),navMain.classList.add("main-nav--closed"))});
